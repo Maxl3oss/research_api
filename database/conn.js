92cef3056f83b96cf661f1c7a9a4f0e9229b8cc0
maxl3oss <maxl3oss@gmail.com>
2022-12-19 05:18:12 +0700

    test cyclic commit

--- database/conn.js
+++ database/conn.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.host,
-  user: process.env.username,
-  password: process.env.password,
-  database: process.env.project_research,
-});
-// const db = mysql.createConnection(process.env.DATABASE_URL);
+// const db = mysql.createConnection({
+//   host: "172.17.0.1",
+//   user: "root",
+//   password: "bPFcM0jB7jnH5ZYN",
+//   database: "project_research",
+// });
+const db = mysql.createConnection('mysql://cmd0b00q74bd7iq2tepi:pscale_pw_QuFoYAZRyfGS0PrJeoOUDWQRFsG8jwEebQdksaRWnv9@ap-northeast.connect.psdb.cloud/project_research?ssl={"rejectUnauthorized":true}');
 
 db.connect((err) => {
   if (err) {
